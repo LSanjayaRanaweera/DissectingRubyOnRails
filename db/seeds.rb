bcc1530a83ceee56fff33a3f5ae28cb2ea43ce23
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-02 12:52:52 -0500

    Created sample data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,31 @@
+10.times do |blog|
+	Blog.create!(
+		title: "My Blog Post #{blog}",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+		)
+end
+puts "10 Blogposts are created"
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percentage_utilized: 15
+		)
+end
+puts "5 skills are created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title #{portfolio_item}",
+		subtitle: "My great service",
+		body: " Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		main_image: "https://via.placeholder.com/600x400",
+		thumb_image: "https://via.placeholder.com/350x200",
+		)
+end
+puts "9 Portfolio items are created"
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
