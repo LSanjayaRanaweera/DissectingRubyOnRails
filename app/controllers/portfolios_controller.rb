ff6057c7e57adc48792c24458ca54b707cab4d62
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-03 11:59:56 -0500

    Implemented destroy#action/delete functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -39,4 +39,13 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.find(params[:id])
 	end
 
+	def destroy
+		@portfolio_item = Portfolio.find(params[:id])
+
+		@portfolio_item.destroy
+		respond_to do |format|
+			format.html{redirect_to portfolios_url, notice: 'The record has been removed'}
+		end
+	end
+
 end
