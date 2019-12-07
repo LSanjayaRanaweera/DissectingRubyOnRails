ede83dd4c3bcc025064f34130359988cbca1cbf7
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-07 15:48:33 -0500

    Implemented toggle status function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   root to: 'pages#home'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
