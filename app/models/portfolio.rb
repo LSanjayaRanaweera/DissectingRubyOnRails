3feca66a76637b7d9a30f971e677edc78021c49d
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-08 11:41:32 -0500

    Implemented image defaults to portfolio-items3

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,4 +8,10 @@ class Portfolio < ApplicationRecord
 	end
 	#Scope Method
 	scope :ruby_on_rails_portfolio_utems, -> {where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+	def set_defaults
+		self.main_image ||= "http://plcehold.it/600x400" #Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= "http://plcehold.it/350x200"#Placeholder.image_generator(height: '350', width: '200')
+	end
 end
