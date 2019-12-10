1adb94169916f7aead8c5bf58873150aaa369d5c
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-10 15:13:16 -0500

    Integrated Nested Attributes for Portfolio Model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,12 @@
 class Portfolio < ApplicationRecord
+	#Parent/Child relationship with Technology table
 	has_many :technologies
+	accepts_nested_attributes_for :technologies, 
+								 reject_if: lambda{|attrs| attrs['name'].blank?}
+
+	#MODEL/CONCERN
 	include Placeholder
+	#Controller Create#action Requirements
 	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	#For DB queries
@@ -11,6 +17,7 @@ class Portfolio < ApplicationRecord
 	#Scope Method
 	scope :ruby_on_rails_portfolio_utems, -> {where(subtitle: 'Ruby on Rails')}
 
+	#Default Image setting >> Method in Model/concern
 	after_initialize :set_defaults
 	def set_defaults
 		self.main_image ||= Placeholder.image_generator(height: '600', width: '400') #"http://plcehold.it/600x400"
