9ac96a76134063ef4abd79165c11435011c2542c
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-18 12:15:07 -0500

    Completed authentication with virtual attributes for user model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -6,7 +6,7 @@ module DeviseWhitelist
 	end
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-		devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
 	end 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  def first_name
+  	self.name.split.first              ## "self==current_user"
+  end
+
+  def last_name
+  	self.name.split.last
+  end
 end
