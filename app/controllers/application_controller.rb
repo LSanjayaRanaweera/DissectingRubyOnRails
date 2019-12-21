522de71cac680acb518e2860b45be51630b7673e
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-21 13:32:48 -0500

    Added session tacker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
 	##This line was ONLY present in Tutorial as a Default setting!!
 	##Added just to mirror the code!! 
 	protect_from_forgery with: :exception 
-    include DeviseWhitelist      
+    
+    include DeviseWhitelist 
+
+    before_action :set_source
+    def set_source
+    	session[:source] = params[:q] if params[:q]
+    end     
 
 end
