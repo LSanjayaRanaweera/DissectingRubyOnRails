3183965d1dbedb5c68a5cb1a81796afeda53aff1
Sanjaya <lsanjayaranaweera@gmail.com>
2019-12-21 16:07:30 -0500

    Refactered set_source method to a controller/concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,8 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception 
     
     include DeviseWhitelist 
+    include SetSource
 
-    before_action :set_source
-    def set_source
-    	session[:source] = params[:q] if params[:q]
-    end     
+    
 
 end
